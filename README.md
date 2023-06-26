d4d552cbd679f3b345134c9c34d962641fc5a26b
Aurora Stone <aurorastone@MacBook-Pro.lan>
2023-06-26 18:59:36 -0400

    edits

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HW11-Ecommerce-Back-End
+
+I made an ecommerce rest API for this assignment focusing on GET/POST/UPDATE/DELETe routes for Product, Category, and Tag resources. 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,20 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+ Product.findAll({
+  include: [Category, {model: Tag, through: ProductTag}]
+ }) .then ((products) => {
+ res.json(products)
+ })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+   Product.findOne({where: {id: req.params.id}}, {
+    include: [Category, {model: Tag, through: ProductTag}]
+   }) .then ((products) => {
+   res.json(products)
+   })
 });
 
 // create new product
@@ -93,7 +99,9 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
-});
+Product.destroy({where: {id: req.params.id}})
+.then((product) => {
+  res.json(product)
+})});
 
 module.exports = router;
